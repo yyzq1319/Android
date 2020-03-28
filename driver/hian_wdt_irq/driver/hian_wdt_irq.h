758094717dddb7d18b255442404ce13faa18958f
yyzq1319 <82344735@qq.com>
2020-03-28 18:09:01 +0800

    合入wdt_irq代码

---
+++ driver/hian_wdt_irq/driver/hian_wdt_irq.h
@@ -0,0 +1,49 @@
+#ifndef _HIAN_WDT_IRQ_H_
+#define _HIAN_WDT_IRQ_H_
+
+#define GPIO_GPC0_3 0
+#define GPIO_GPX0_6 1
+
+#ifndef DEVICE_NAME
+#define DEVICE_NAME "hian_wdt"
+#endif
+
+#define DRIVER_NAME "hian_wdt"
+
+#ifndef DEVICE_MINOR_NUM
+#define DEVICE_MINOR_NUM 1
+#endif
+
+#ifndef DEV_MAJOR
+#define DEV_MAJOR 0
+#endif
+
+#ifndef DEV_MINOR
+#define DEV_MINOR 0
+#endif
+
+#ifndef REGDEV_SIZE
+#define REGDEV_SIZE 3000
+#endif
+
+#define EXYNOS4_PA_WDT 0x10060000
+#define WTCON      0x00
+#define WTCNT      0x08
+#define WTDAT      0x04
+#define WTCLEARINT 0x0c
+
+struct wdt_st{
+	int wdtcon, wdtdat, wdtcnt;
+	void *__iomem v;
+	struct clk *clk;
+	
+	struct cdev dev;
+	
+	spinlock_t lock;
+	int count;
+};
+
+struct wdt_feed_st{
+	unsigned int times;
+};
+#endif
--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ date
 echo "code add......"
 git add .
 echo "code commit......"
-git commit -m "合入wdt和seq_file_list驱动，其中seq_file_list有问题，需要定位"
+git commit -m "合入wdt_irq代码"
 echo "code push up......"
 git push -u origin master
 
