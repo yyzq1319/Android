b1c1c6419f9539d32bf7dfa024de2027cca8af7c
yyzq1319 <82344735@qq.com>
2020-03-29 17:39:26 +0800

    合入key irq work queue代码

--- git.sh
+++ git.sh
@@ -3,7 +3,7 @@ date
 echo "code add......"
 git add .
 echo "code commit......"
-git commit -m "合入key irq tasklet代码"
+git commit -m "合入key irq work queue代码"
 echo "code push up......"
 git push -u origin master
 
